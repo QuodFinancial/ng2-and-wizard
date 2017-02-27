84ab96378be6d3c02753a3bcff14376afb431dce
Thomas Kerbrat <kerbrat@outlook.com>
2017-02-27 14:32:57 +0100

    Import the FormsModule in the Players module

--- src/app/players/players.module.ts
+++ src/app/players/players.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MaterialModule } from '@angular/material';
+import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { playersRouting } from './players-routing.module';
@@ -11,6 +12,7 @@ import { PlayersListComponent } from './players-list/players-list.component';
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     MaterialModule,
     RouterModule.forRoot(playersRouting),
   ],
