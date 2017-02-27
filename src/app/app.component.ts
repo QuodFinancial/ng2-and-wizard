69586543dd5932b68614dd1ff76a1dc01093ab50
Thomas Kerbrat <kerbrat@outlook.com>
2017-02-27 12:05:38 +0100

    Declare the PlayersService in the PlayersComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
-import { PlayersService } from './players.service';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [PlayersService]
+  providers: []
 })
 export class AppComponent { }
--- src/app/players/players.component.ts
+++ src/app/players/players.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Player } from '../../player';
-import { PlayersService } from '../../players.service';
+import { Player } from './shared/player';
+import { PlayersService } from './shared/players.service';
 
 @Component({
   selector: 'app-players',
   templateUrl: './players.component.html',
-  styleUrls: ['./players.component.css']
+  styleUrls: ['./players.component.css'],
+  providers: [PlayersService],
 })
 export class PlayersComponent implements OnInit {
 
